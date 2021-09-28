c308719e90f1abd2be5c1abe7b630af3be5458d9
mickym23 <mikhail.cedras@gmail.com>
2021-09-29 01:09:06 +0200

    checkpoint03

---
+++ checkpoint03.py
@@ -0,0 +1,9 @@
+userAge = int(input('How old are you? '))
+print('On your next birthday, you will be ' + str(userAge + 1) + '\n')
+
+eggCartons = int(input('\nHow many egg cartons do you have? '))
+print('You have ' + str(eggCartons * 12) + ' eggs')
+
+cookies = int(input('\nHow many cookies do you have? '))
+people = int(input('How many people are there? '))
+print('Each person may have ' + str(cookies/people) + ' cookies')
