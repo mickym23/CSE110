6d7eb840299f80767ffac400b22daed9b68b6de9
mickym23 <mikhail.cedras@gmail.com>
2021-10-29 16:02:24 +0200

    Added Prove Assignment

--- Week7/cse110_images/prove07.py
+++ Week7/cse110_images/prove07.py
@@ -9,11 +9,12 @@ image_original.show()
 
 width, height = image_original.size
 
+# Print the width and height of the current image
 print(width, height)
-
 
 pixels_original = image_original.load()
 
+# Printing RGB values of pixel (1, 1)
 print(pixels_original[1,1])
 
 r, g, b = pixels_original[100, 200]
@@ -31,11 +32,12 @@ print(f'Pixel #4: ', r, g, b)
 r, g, b = pixels_original[100, 200]
 print(f'Pixel #5: ', r, g, b)
 
-# Don't forget to use parentheses around your (r, g, b)
+# Setting pixels in the same 'column' to black so that it is easy to see the changes
 pixels_original[100, 200] = (0, 0, 0)
 pixels_original[100, 201] = (0, 0, 0)
 pixels_original[100, 202] = (0, 0, 0)
 pixels_original[100, 203] = (0, 0, 0)
 pixels_original[100, 204] = (0, 0, 0)
 
+# Saving to output image file
 image_original.save("output_file.jpg")
