6dd8d2d8afadf7189996a0ea4df229299344dccf
mickym23 <mikhail.cedras@gmail.com>
2021-11-05 05:25:26 +0200

    Added TA08

---
+++ Week8/ta08.py
@@ -0,0 +1,11 @@
+cols_and_rows = int(input('How many columns and rows do you want in your multiplication table? '))
+
+for row in range(1, cols_and_rows + 1):
+   for column in range(1, cols_and_rows + 1):
+      if ((cols_and_rows * cols_and_rows) >= 100):
+        print(f"{(row*column):3}", end=" ")
+      else:
+        print(f"{(row*column):2}", end=" ")
+   print()
+   
+   
