c1da4082c6a4fc7c29216f8414820c9e61cb6931
mickym23 <mikhail.cedras@gmail.com>
2021-11-06 16:23:18 +0200

    Added Prove 08

--- Week8/ta08.py
+++ Week8/ta08.py
@@ -1,5 +1,4 @@
 cols_and_rows = int(input('How many columns and rows do you want in your multiplication table? '))
-
 for row in range(1, cols_and_rows + 1):
    for column in range(1, cols_and_rows + 1):
       if ((cols_and_rows * cols_and_rows) >= 100):
