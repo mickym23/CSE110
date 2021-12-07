24c96f69b65e84aeeb215f023b4a11e7a7a07134
mickym23 <mikhail.cedras@gmail.com>
2021-12-07 15:58:51 +0200

    Added Checkpoint13

---
+++ Week13/checkpoint13.py
@@ -0,0 +1,15 @@
+def display_regular(value):
+   print(value)
+
+def display_uppercase(value):
+   print(value.upper())
+
+def display_lowercase(value):
+   print(value.lower())
+
+message = input(f'What is your message? ')
+
+display_regular(message)
+display_uppercase(message)
+display_lowercase(message)
+
